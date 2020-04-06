21e7340de3bc7e0516ddc1315eb1b912f402db45
Javier Botero <javierbotero10271@gmail.com>
2020-04-06 17:18:02 -0500

    Getting first pull form the page. Needs better code to just pull what it is required

--- scraper.rb
+++ scraper.rb
@@ -1,4 +1,7 @@
 require 'nokogiri'
 require 'open-uri'
 
-doc = Nokogiri::HTML(open('https://'))
+doc = Nokogiri::HTML(open('https://importacionesarturia.com/categoria/camaras-fotografia-y-video/camaras-nikon/'))
+dslrcams = doc.css('mf-shop-content li a href')
+puts dslrcams
+
