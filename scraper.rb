0651e3181661ac3f5a85a7148b738e84f021764d
Javier Botero <javierbotero10271@gmail.com>
2020-04-06 23:20:00 -0500

    Add handlers for name of cameras

--- scraper.rb
+++ scraper.rb
@@ -2,6 +2,15 @@ require 'nokogiri'
 require 'open-uri'
 
 doc = Nokogiri::HTML(open('https://importacionesarturia.com/categoria/camaras-fotografia-y-video/camaras-nikon/'))
-dslrcams = doc.css('mf-shop-content li a href')
-puts dslrcams
+dslrcams = doc.search("ul[class='products columns-4']")
+information = dslrcams.search("div[class='mf-product-details']")
+information.each do |x|    
+  prices = {}
+  div_info = x.search("div[class='mf-product-content']")
+  heading_info = div_info.search("h2")
+  info_product = heading_info.search("a")
+  name_product = info_product.text
+  puts price = x.xpath("//div[class='mf-product-price-box']//span[class='price']")
+end
+
 
