7e5f96a8730ce2ccc1e31679f662c81e8cf8abf4
Javier Botero <javierbotero10271@gmail.com>
2020-04-12 22:59:53 -0500

    Refactoring code, cleaning linting errors

--- lib/module_nodes.rb
+++ lib/module_nodes.rb
@@ -1,34 +1,24 @@
 module NodesPage
-    DOCUMENT = Nokogiri::HTML(open('https://importacionesarturia.com/tienda/'))
-    NODE_BASE = DOCUMENT.search("ul[class='product-categories']")
-    AUDIO = NODE_BASE.search("li[class='cat-item cat-item-48']").search("ul[class='children']").search('li')
-    VIDEO = NODE_BASE.search("li[class='cat-item cat-item-42']").search("ul[class='children']").search('li')
-    GPS = NODE_BASE.search("li[class='cat-item cat-item-62']").search("ul[class='children']").search('li')
-    
-    def self.convert_string(x)
-      case x
-      when 'á'
-        'a'
-      when 'é'
-        'e'
-      when 'í'
-        'i'
-      when 'ó'
-        'o'
-      when 'ú'
-        'u'
-      when ' '
-        '_'
-      when /[A-Z]/
-        x.downcase
-      else
-        x
-      end
-    end
+  DOCUMENT = Nokogiri::HTML(open('https://importacionesarturia.com/tienda/'))
+  NODE_BASE = DOCUMENT.search("ul[class='product-categories']")
+  AUDIO = NODE_BASE.search("li[class='cat-item cat-item-48']").search("ul[class='children']").search('li')
+  VIDEO = NODE_BASE.search("li[class='cat-item cat-item-42']").search("ul[class='children']").search('li')
+  GPS = NODE_BASE.search("li[class='cat-item cat-item-62']").search("ul[class='children']").search('li')
 
-    def self.class_name(array)
-      array = array.join
-      first_part = array[0].upcase
-      name_class = first_part + array[1..-1]
-    end
+  def self.convert_string(letter)    
+    return 'a' if letter == 'á'
+    return 'e' if letter == 'é'
+    return 'i' if letter == 'í'
+    return 'o' if letter == 'ó'
+    return 'u' if letter == 'ú'
+    return '_' if letter == ' '
+    return letter.downcase if /[A-Z]/.match(letter)
+    letter
+  end
+
+  def self.class_name(array)
+    array = array.join
+    first_part = array[0].upcase
+    name_class = first_part + array[1..-1]
+  end
 end
